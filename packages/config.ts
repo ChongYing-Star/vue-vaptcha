475564c2e384a18426f0a8551e592931ff1a098a
Tm-SunnyDay <1581272410@qq.com>
2023-04-06 15:44:58 +0800

    chore: 允许使用更多默认选项

--- packages/config.ts
+++ packages/config.ts
@@ -1,6 +1,6 @@
-import { VaptchaOption } from '@chongying-star/vaptcha-typescript';
+import { VaptchaOptionClickType, VaptchaOptionEmbeddedType, VaptchaOptionInvisibleType } from '@chongying-star/vaptcha-typescript';
 
-type Option = Pick<VaptchaOption, 'vid' | 'area' | 'lang'>;
+export type Option = Omit<VaptchaOptionClickType, 'mode'|'container'> & Omit<VaptchaOptionEmbeddedType, 'mode'|'container'> & Omit<VaptchaOptionInvisibleType, 'mode'>;
 
 const _option: Partial<Option> = {};
 export const option: Readonly<Partial<Option>> = new Proxy(_option, {
